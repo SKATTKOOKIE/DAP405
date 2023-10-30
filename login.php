7a040fda2ec7d699fadda9e5d7f9af99e6ea0cd3
Brandon <118194245+SKATTKOOKIE@users.noreply.github.com>
2023-10-30 09:24:27 +0000

    first commit - added login form to direct user into companies pay roll system

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Woodton LTD Login</title>
+    <link rel="stylesheet" href="stylesheets/mainstyle.css">
+</head>
+<body>
+<div class="login-container">
+    <h2>Login</h2>
+    <?php
+    // Check for an error message in the URL
+    if (isset($_GET['error']) && $_GET['error'] == 1) {
+        echo '<p class="error-message">Username or password is incorrect. Please try again.</p>';
+    }
+    ?>
+    <form class="login-form" method="post" action="login-process.php">
+        <div class="input-group">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div class="input-group">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div class="input-group">
+            <input type="submit" value="Login">
+        </div>
+        <div class="input-group">
+            <p>HINT!!!!! Username: "root"   |   Password: "password" </p>
+        </div>
+    </form>
+</div>
+</body>
+</html>
